c2b41e6b41afd55ed69ea68fa452ca81156aec40
coryb08 <douglasb2008@gmail.com>
2017-10-05 23:34:16 -0400

    Done.

--- hashketball.rb
+++ hashketball.rb
@@ -1,11 +1,39 @@
-# Write your code here!
+def game_hash
+  hash = {
+    home: {
+      team_name: "Brooklyn Nets",
+      colors:['Black', 'White'],
+      players:{
+        name: {
+          number: ""
+          shoe:""
+          points:""
+          rebounds:""
+          assists:""
+          steals:""
+          blocks:""
+          slam_dunks:""
+        }
+      }
+    },
 
+    away: {
+      team_name: "",
+      colors:[],
+      players:{
+        name: {
+          number: ""
+          shoe:""
+          points:""
+          rebounds:""
+          assists:""
+          steals:""
+          blocks:""
+          slam_dunks:""
+        }
+      }
+    }
+  }
 
-
-
-
-
-
-
-
-
+hash
+end
